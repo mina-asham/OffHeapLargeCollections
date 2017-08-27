bb5b602224a79edcbcf141995506f56306134475
Mina Asham <mina.asham@hotmail.com>
2017-08-27 19:38:09 +0100

    Add get and put wrappers in UnsafeUtils for byte, short, char, int, long, float, and double

--- src/main/java/com/github/minaasham/offheap/largecollections/UnsafeUtils.java
+++ src/main/java/com/github/minaasham/offheap/largecollections/UnsafeUtils.java
@@ -8,7 +8,7 @@ import java.lang.reflect.Field;
 
 /**
  * UnsafeUtils, a simple wrapper around the Unsafe object
- * It handles allocation and freeing of memory
+ * It handles allocation and freeing of memory, as well as reading and writing native types
  * This class is package private as it's an internal implementation detail
  */
 @UtilityClass
@@ -38,6 +38,146 @@ final class UnsafeUtils {
      */
     static void free(long address) {
         UNSAFE.freeMemory(address);
+    }
+
+    /**
+     * Reads a byte from the specific memory address
+     *
+     * @param address The address to read the byte from
+     * @return The byte read
+     */
+    static byte getByte(long address) {
+        return UNSAFE.getByte(address);
+    }
+
+    /**
+     * Writes a byte to the specific memory address
+     *
+     * @param address The address to write the byte at
+     * @param value   The byte value
+     */
+    static void putByte(long address, byte value) {
+        UNSAFE.putByte(address, value);
+    }
+
+    /**
+     * Reads a short from the specific memory address
+     *
+     * @param address The address to read the short from
+     * @return The short read
+     */
+    static short getShort(long address) {
+        return UNSAFE.getShort(address);
+    }
+
+    /**
+     * Writes a short to the specific memory address
+     *
+     * @param address The address to write the short at
+     * @param value   The short value
+     */
+    static void putShort(long address, short value) {
+        UNSAFE.putShort(address, value);
+    }
+
+    /**
+     * Reads a character from the specific memory address
+     *
+     * @param address The address to read the character from
+     * @return The character read
+     */
+    static char getChar(long address) {
+        return UNSAFE.getChar(address);
+    }
+
+    /**
+     * Writes a character to the specific memory address
+     *
+     * @param address The address to write the character at
+     * @param value   The character value
+     */
+    static void putChar(long address, char value) {
+        UNSAFE.putChar(address, value);
+    }
+
+    /**
+     * Reads an integer from the specific memory address
+     *
+     * @param address The address to read the integer from
+     * @return The integer read
+     */
+    static int getInt(long address) {
+        return UNSAFE.getInt(address);
+    }
+
+    /**
+     * Writes a integer to the specific memory address
+     *
+     * @param address The address to write the integer at
+     * @param value   The integer value
+     */
+    static void putInt(long address, int value) {
+        UNSAFE.putInt(address, value);
+    }
+
+    /**
+     * Reads a long from the specific memory address
+     *
+     * @param address The address to read the long from
+     * @return The long read
+     */
+    static long getLong(long address) {
+        return UNSAFE.getLong(address);
+    }
+
+    /**
+     * Writes a long to the specific memory address
+     *
+     * @param address The address to write the long at
+     * @param value   The long value
+     */
+    static void putLong(long address, long value) {
+        UNSAFE.putLong(address, value);
+    }
+
+    /**
+     * Reads a float from the specific memory address
+     *
+     * @param address The address to read the float from
+     * @return The float read
+     */
+    static float getFloat(long address) {
+        return UNSAFE.getFloat(address);
+    }
+
+    /**
+     * Writes a float to the specific memory address
+     *
+     * @param address The address to write the float at
+     * @param value   The float value
+     */
+    static void putFloat(long address, float value) {
+        UNSAFE.putFloat(address, value);
+    }
+
+    /**
+     * Reads a double from the specific memory address
+     *
+     * @param address The address to read the double from
+     * @return The double read
+     */
+    static double getDouble(long address) {
+        return UNSAFE.getDouble(address);
+    }
+
+    /**
+     * Writes a double to the specific memory address
+     *
+     * @param address The address to write the double at
+     * @param value   The double value
+     */
+    static void putDouble(long address, double value) {
+        UNSAFE.putDouble(address, value);
     }
 
     /**
--- src/test/java/com/github/minaasham/offheap/largecollections/UnsafeUtilsTest.java
+++ src/test/java/com/github/minaasham/offheap/largecollections/UnsafeUtilsTest.java
@@ -7,10 +7,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import sun.misc.Unsafe;
 
+import java.util.Random;
+
 import static org.junit.Assert.assertEquals;
 
 @RunWith(JMockit.class)
 public class UnsafeUtilsTest {
+
+    private static final Random RANDOM = new Random();
+    private static final double DELTA = 1e-10;
 
     @Test
     public void testAllocate() {
@@ -46,5 +51,68 @@ public class UnsafeUtilsTest {
         };
 
         UnsafeUtils.free(expectedAddress);
+    }
+
+    @Test
+    public void testByte() {
+        long address = UnsafeUtils.allocate(Byte.BYTES);
+        byte value = (byte) RANDOM.nextInt();
+        UnsafeUtils.putByte(address, value);
+        assertEquals(value, UnsafeUtils.getByte(address));
+        UnsafeUtils.free(address);
+    }
+
+    @Test
+    public void testShort() {
+        long address = UnsafeUtils.allocate(Short.BYTES);
+        short value = (short) RANDOM.nextInt();
+        UnsafeUtils.putShort(address, value);
+        assertEquals(value, UnsafeUtils.getShort(address));
+        UnsafeUtils.free(address);
+    }
+
+    @Test
+    public void testChar() {
+        long address = UnsafeUtils.allocate(Character.BYTES);
+        char value = (char) RANDOM.nextInt();
+        UnsafeUtils.putChar(address, value);
+        assertEquals(value, UnsafeUtils.getChar(address));
+        UnsafeUtils.free(address);
+    }
+
+    @Test
+    public void testInt() {
+        long address = UnsafeUtils.allocate(Integer.BYTES);
+        int value = RANDOM.nextInt();
+        UnsafeUtils.putInt(address, value);
+        assertEquals(value, UnsafeUtils.getInt(address));
+        UnsafeUtils.free(address);
+    }
+
+    @Test
+    public void testLong() {
+        long address = UnsafeUtils.allocate(Long.BYTES);
+        long value = RANDOM.nextLong();
+        UnsafeUtils.putLong(address, value);
+        assertEquals(value, UnsafeUtils.getLong(address));
+        UnsafeUtils.free(address);
+    }
+
+    @Test
+    public void testFloat() {
+        long address = UnsafeUtils.allocate(Float.BYTES);
+        float value = RANDOM.nextFloat();
+        UnsafeUtils.putFloat(address, value);
+        assertEquals(value, UnsafeUtils.getFloat(address), DELTA);
+        UnsafeUtils.free(address);
+    }
+
+    @Test
+    public void testDouble() {
+        long address = UnsafeUtils.allocate(Double.BYTES);
+        double value = RANDOM.nextDouble();
+        UnsafeUtils.putDouble(address, value);
+        assertEquals(value, UnsafeUtils.getDouble(address), DELTA);
+        UnsafeUtils.free(address);
     }
 }
