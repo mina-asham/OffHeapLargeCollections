1d6362680baf9234dcb155e75e52a7120c8bec42
Mina Asham <mina.asham@hotmail.com>
2017-08-31 18:44:22 +0100

    Add LargeMap; the main interface for off heap large maps

---
+++ src/main/java/com/github/minaasham/offheap/largecollections/LargeMap.java
@@ -0,0 +1,49 @@
+package com.github.minaasham.offheap.largecollections;
+
+import java.util.Map.Entry;
+
+/**
+ * LargeMap, the main interface for all open head large maps
+ *
+ * @param <K> The key type
+ * @param <V> The value type
+ */
+public interface LargeMap<K, V> extends Iterable<Entry<K, V>>, AutoCloseable {
+
+    /**
+     * Gets key's value from the map
+     *
+     * @param key The key to lookup
+     * @return The value associated with the key
+     */
+    V get(K key);
+
+    /**
+     * Puts the key and value in the map
+     *
+     * @param key   The key to insert in the map
+     * @param value The value to insert in the map
+     * @return The old value related to that key
+     */
+    V put(K key, V value);
+
+    /**
+     * Removes the key from the map if it exists
+     *
+     * @param key The key to remove from the map
+     * @return The value of the key
+     */
+    V remove(K key);
+
+    /**
+     * Clear the map from all keys and values
+     */
+    void clear();
+
+    /**
+     * Gets the current size of the map
+     *
+     * @return The size of the map
+     */
+    long size();
+}
