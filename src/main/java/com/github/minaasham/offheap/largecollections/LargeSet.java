55d52d0703c8b3fd37f51598d4ca4b1cb43bb63e
Mina Asham <mina.asham@hotmail.com>
2017-09-04 22:38:24 +0100

    Add LargeSet interface, this is the main interface for large sets

---
+++ src/main/java/com/github/minaasham/offheap/largecollections/LargeSet.java
@@ -0,0 +1,45 @@
+package com.github.minaasham.offheap.largecollections;
+
+/**
+ * LargeSet, the main interface for all large sets
+ *
+ * @param <E> The element type
+ */
+public interface LargeSet<E> extends Iterable<E>, AutoCloseable {
+
+    /**
+     * Returns <tt>true</tt> if this set contains the specified element
+     *
+     * @param element Element whose presence in this set is to be tested
+     * @return <tt>true</tt> if this set contains the specified element
+     */
+    boolean contains(E element);
+
+    /**
+     * Adds the specified element to this set if it is not already present
+     *
+     * @param element Element to be added to this set
+     * @return <tt>true</tt> if this set did not already contain the specified element
+     */
+    boolean add(E element);
+
+    /**
+     * Removes the specified element from this set if it is present
+     *
+     * @param element Element to be removed from this set, if present
+     * @return <tt>true</tt> if this set contained the specified element
+     */
+    boolean remove(E element);
+
+    /**
+     * Clear the set from all elements
+     */
+    void clear();
+
+    /**
+     * Gets the current size of the set
+     *
+     * @return The size of the set
+     */
+    long size();
+}
